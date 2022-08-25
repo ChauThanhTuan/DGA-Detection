495649561224439929515fc8ddd3199a5bbff2e5
TT <54493212+Tuan164@users.noreply.github.com>
2022-08-25 20:47:54 +0700

    Delete entrypoint.sh

--- SELKS/docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# curl -X POST "http://172.16.60.10:5601/api/index_patterns/index_pattern" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d' { "index_pattern": { "title": "tt" } } '
-echo "${hostname -I}"
-
-text="a"
-text="${text} $(echo "b") c"
-echo "${text}"
