0312ea5025fb48989a4a52cbd4e3ad86c2b1b593
TT <54493212+Tuan164@users.noreply.github.com>
2022-08-29 22:26:53 +0700

    Update entrypoint.sh

--- SELKS/docker/containers-data/DGA/entrypoint.sh
+++ SELKS/docker/containers-data/DGA/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 IP=$(head -n 1 config.py)
-curl -X POST "http://${IP:12:-1}:5601/api/index_patterns/index_pattern" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d' { "index_pattern": { "title": "classifyDomain" } } ' || echo SELKS_DGA
+curl -X POST "http://${IP:12:-1}:5601/api/index_patterns/index_pattern" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d' { "index_pattern": { "title": "classify_domains" } } ' || echo SELKS_DGA
 ./selks_dga.py
