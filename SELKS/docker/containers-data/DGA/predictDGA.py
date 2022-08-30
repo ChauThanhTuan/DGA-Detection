40c0842e765d91ae00f851e74b86e61b4a198846
TT <54493212+Tuan164@users.noreply.github.com>
2022-08-30 09:59:35 +0700

    Update predictDGA.py

--- SELKS/docker/containers-data/DGA/predictDGA.py
+++ SELKS/docker/containers-data/DGA/predictDGA.py
@@ -47,7 +47,7 @@ def _get_prediction(domain_name, prob=None):
             'timestamp': datetime.now() + timedelta(hours=7),
         }
         res = es.index(index="classify_domains", id=count, document=doc)
-        cmd = f'curl -XPOST -H \'Authorization: Bearer dlkOW4xt2XCIUDrinP1B6d5cgb0Vxipv\' -H \'Content-Type: application/json\' http://{config.SELKS_IP}:{config.THEHIVE_PORT}//api/alert -d \'\x7B  "title": "DGA alert",  "description": "DGA alert",  "type": "external",  "source": "instance"{str(datetime.now())},  "sourceRef": "alert-ref"\x7D\''
+        cmd = f'curl -XPOST -H \'Authorization: Bearer dlkOW4xt2XCIUDrinP1B6d5cgb0Vxipv\' -H \'Content-Type: application/json\' http://{config.SELKS_IP}:{config.THEHIVE_PORT}/api/alert -d \'\x7B  "title": "DGA alert",  "description": "DGA alert",  "type": "external",  "source": "instance-{str(datetime.now())}",  "sourceRef": "alert-ref"\x7D\''
         args = shlex.split(cmd)
         process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
