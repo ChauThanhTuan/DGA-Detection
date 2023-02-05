40a8995089c01cd090265f893e57658541dd9940
ChauThanhTuan <54493212+ChauThanhTuan@users.noreply.github.com>
2023-02-05 09:56:53 +0700

    Update getDomain.py

--- SELKS/docker/containers-data/DGA/getDomain.py
+++ SELKS/docker/containers-data/DGA/getDomain.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import config
 
 def getDomains(url):
     domains = []
     headers = {"Content-Type": "application/json"}
-    data = { "aggs": { "0": { "terms": { "field": "dns.rrname.raw", "order": { "1": "desc" }, "size": 10000 }, "aggs": { "1": { "cardinality": { "field": "dns.rrname.raw" } } } } }, "size": 0, "query": { "bool": { "must": [], "filter": [ { "range": { "@timestamp": { "format": "strict_date_optional_time", "gte": "now-15m", "lte": "now" } } } ] } } }
+    data = { "aggs": { "0": { "terms": { "field": "dns.rrname.raw", "order": { "1": "desc" }, "size": 10000 }, "aggs": { "1": { "cardinality": { "field": "dns.rrname.raw" } } } } }, "size": 0, "query": { "bool": { "must": [], "filter": [ { "range": { "@timestamp": { "format": "strict_date_optional_time", "gte": "now-{config.INTERVAL}s", "lte": "now" } } } ] } } }
 
     responses = requests.get(url, headers=headers, data=json.dumps(data)).json()
 
